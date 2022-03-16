b419e793b4412959fc5a55c12a0cb90be9e341af
박우림 <woorimprog@gmail.com>
2022-03-17 04:14:26 +0900

    Update(woorim/swagger): Update the location of the swagger folder to activate

--- app/app.js
+++ app/app.js
@@ -2,4 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(`${__dirname}/src/public`));
+
 app.listen(PORT, () => console.log(`opened Express server on ${PORT}`));
