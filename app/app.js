5a04d2c96cd020a96a2a383cb8cb56173e6b91ba
김지수 <kimjisu3817@gmail.com>
2022-03-18 12:53:29 +0900

    Add filees for initial setup

--- app/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(`${__dirname}/src/public`));
-
-app.listen(PORT, () => console.log(`opened Express server on ${PORT}`));
---
+++ app/src/services/Root/Repository.ts
@@ -0,0 +1,11 @@
+// export default class Repository {
+//   private pool;
+
+//   constructor() {
+//     this.pool = mariadb.getConnection();
+//   }
+
+//   releaseConnection() {
+//     this.pool.release();
+//   }
+// }
