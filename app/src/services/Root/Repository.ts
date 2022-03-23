b1a7aff847178270110e36a4bf9316d0446478ac
김지수 <kimjisu3817@gmail.com>
2022-03-23 22:04:08 +0900

    Add the mariadb config file

--- app/src/services/Root/Repository.ts
+++ app/src/services/Root/Repository.ts
@@ -1,3 +1,13 @@
-export default class Repository {
+// import mariaDB from '../../config/mariadb';
 
-}
+// export default class Repository {
+//   private pool;
+
+//   constructor() {
+//     this.pool = mariaDB.getConnection();
+//   }
+
+//   async releaseConnection(): Promise<void> {
+//     (await this.pool).release();
+//   }
+// }
